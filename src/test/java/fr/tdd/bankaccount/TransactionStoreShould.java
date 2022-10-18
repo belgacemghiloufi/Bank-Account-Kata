1b44aa1bc2eb2d7859d275915929890ba05bf0d3
ghiloufi <belgacemghiloufi2021@gmail.com>
2022-10-18 17:03:20 +0200

    Add A Unit Test For The TransactionStore AddWithdrawTransaction Method

--- src/test/java/fr/tdd/bankaccount/TransactionStoreShould.java
+++ src/test/java/fr/tdd/bankaccount/TransactionStoreShould.java
@@ -14,24 +14,33 @@ import org.mockito.junit.MockitoJUnitRunner;
 
 @RunWith(MockitoJUnitRunner.class)
 public class TransactionStoreShould {
-	
+
 	private static final String TODAY = "18/10/2022";
-	
+
 	private TransactionStore transactionStore;
-	@Mock private Clock clock;
-	
+	@Mock
+	private Clock clock;
+
 	@Before
 	public void initialise() {
 		given(clock.getTodayAsString()).willReturn(TODAY);
 		transactionStore = new TransactionStore(clock);
 	}
-	
+
 	@Test
 	public void create_a_deposit_transaction() {
 		transactionStore.addDepositTransaction(100);
 		List<Transaction> transactions = transactionStore.getTransactions();
 		assertThat(transactions.size(), is(1));
 		assertThat(transactions.get(0), is(transaction(TODAY, 100)));
+	}
+
+	@Test
+	public void create_a_withdraw_transaction() {
+		transactionStore.addWithdrawTransaction(100);
+		List<Transaction> transactions = transactionStore.getTransactions();
+		assertThat(transactions.size(), is(1));
+		assertThat(transactions.get(0), is(transaction(TODAY, -100)));
 	}
 
 	private Transaction transaction(String date, int amount) {
