7139c269be4e00009000f47e38324c49edd1a3d6
ghiloufi <belgacemghiloufi2021@gmail.com>
2022-10-18 17:13:39 +0200

    Add A Unit Test For The Clock GetTodayAsString Method

--- src/main/java/fr/tdd/bankaccount/Clock.java
+++ src/main/java/fr/tdd/bankaccount/Clock.java
@@ -1,8 +1,14 @@
 package fr.tdd.bankaccount;
 
+import java.time.LocalDate;
+
 public class Clock {
 
 	public String getTodayAsString() {
+		throw new UnsupportedOperationException();
+	}
+
+	public LocalDate today() {
 		throw new UnsupportedOperationException();
 	}
 
